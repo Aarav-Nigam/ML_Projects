5bff113d641d4bb71627f7f212432e33e576ae06
Aarav-Nigam <avatar.aarav28@gmail.com>
2023-07-31 16:55:35 +0530

    adding serach functionality

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Grid, Paper } from '@mui/material'
 import ProjCard from '../Proj_Card/Proj_Card'
 import { List } from './List'
-function Gallery() {
+function Gallery({search}) {
   return (
     <Paper elevation={24} className='bg-primary m-5 p-5'>
         <Grid container spacing={2}>
         {
-            List.map(
+            List.filter((o)=>{
+                if(o.name.toLowerCase().includes(search.toLowerCase())) return true;
+                else return false;
+            }).map(
                 (obj)=>{return (
                     <Grid item>
                         <ProjCard details={obj}/>
